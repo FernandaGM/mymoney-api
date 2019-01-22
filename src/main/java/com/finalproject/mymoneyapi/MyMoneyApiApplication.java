d263824e560fab8294d0d1473f7ac0ce4dea04df
FernandaGM <fernanda.moreira@viasoft.com.br>
2019-01-22 16:38:44 -0200

    Initial repository load

---
+++ src/main/java/com/finalproject/mymoneyapi/MyMoneyApiApplication.java
@@ -0,0 +1,14 @@
+package com.finalproject.mymoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoneyApiApplication.class, args);
+	}
+
+}
+
