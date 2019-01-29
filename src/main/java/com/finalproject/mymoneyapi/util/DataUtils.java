7a946f53cea545b255d2a13df9566e9ff1ce9b1b
FernandaGM <fernanda.moreira@viasoft.com.br>
2019-01-29 18:00:15 -0200

    DataUtil - implementação do dateRange

--- src/main/java/com/finalproject/mymoneyapi/util/DataUtils.java
+++ src/main/java/com/finalproject/mymoneyapi/util/DataUtils.java
@@ -1,7 +1,11 @@
 package com.finalproject.mymoneyapi.util;
 
+import org.springframework.data.util.Pair;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DataUtils {
 
@@ -13,5 +17,51 @@ public class DataUtils {
     public static String formatDateWithTime(Date data) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/M/yyyy HH:mm:ss");
         return dateFormat.format(data);
+    }
+
+    public static Pair<Date, Date> getDateRangeFromYearMonth(int month, int year) {
+        Date begining, end;
+
+        {
+            Calendar calendar = getCalendarForNow();
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.DAY_OF_MONTH,
+                    calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+            setTimeToBeginningOfDay(calendar);
+            begining = calendar.getTime();
+        }
+
+        {
+            Calendar calendar = getCalendarForNow();
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.DAY_OF_MONTH,
+                    calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+            setTimeToEndofDay(calendar);
+            end = calendar.getTime();
+        }
+
+        return Pair.of(begining, end);
+    }
+
+    private static Calendar getCalendarForNow() {
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTime(new Date());
+        return calendar;
+    }
+
+    private static void setTimeToBeginningOfDay(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+    private static void setTimeToEndofDay(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
     }
 }
